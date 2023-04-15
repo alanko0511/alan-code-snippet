c37991a897be411e8a012dc3626c4a754d67db17
Alan Ko <34646302+alanko0511@users.noreply.github.com>
2023-04-15 17:57:40 -0400

    Remove extra key generate & add mkdir

--- deno/generate-keys.ts
+++ deno/generate-keys.ts
@@ -1,27 +1,20 @@
 #!/usr/bin/env -S deno run --allow-write
 
-const exportJWK = (key: CryptoKey) => crypto.subtle.exportKey('jwk', key);
+const exportJWK = (key: CryptoKey) => crypto.subtle.exportKey("jwk", key);
 
 // Generate JWT verification key
 const jwtKey = await crypto.subtle.generateKey(
   {
-    name: 'HMAC',
-    hash: { name: 'SHA-512' },
+    name: "HMAC",
+    hash: { name: "SHA-512" },
   },
   true,
-  ['sign', 'verify']
+  ["sign", "verify"],
 );
 
-await Deno.writeTextFile('./keys/jwt.json', JSON.stringify(await exportJWK(jwtKey), null, 2));
+await Deno.mkdir("./keys", { recursive: true });
 
-// Generate session key
-const sessionKey = await crypto.subtle.generateKey(
-  {
-    name: 'AES-GCM',
-    length: 256,
-  },
-  true,
-  ['encrypt', 'decrypt']
+await Deno.writeTextFile(
+  "./keys/jwt.json",
+  JSON.stringify(await exportJWK(jwtKey), null, 2),
 );
-
-await Deno.writeTextFile('./keys/session-key.json', JSON.stringify(await exportJWK(sessionKey), null, 2));
